1c2f1ac72220212388a8be7ae7ba8e7cee9e79fd
h3nrey <pedro.novaes.education@gmail.com>
2025-11-30 17:54:22 -0300

    feat: add prisma service as a app provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
+import { PrismaService } from './prisma/prisma.service';
+
 @Module({
   imports: [UsersModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
