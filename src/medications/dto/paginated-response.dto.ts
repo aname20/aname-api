e0229d7b77268e54e0d2e5502adc13b524613f77
pedorooo <150040569+pedorooo@users.noreply.github.com>
2025-12-06 17:34:27 -0300

    feat: crud básico de medicamentos

---
+++ src/medications/dto/paginated-response.dto.ts
@@ -0,0 +1,23 @@
+export class PaginatedResponseDto<T> {
+  data: T[];
+  pagination: {
+    total: number;
+    page: number;
+    limit: number;
+    totalPages: number;
+    hasNextPage: boolean;
+    hasPreviousPage: boolean;
+  };
+
+  constructor(data: T[], total: number, page: number, limit: number) {
+    this.data = data;
+    this.pagination = {
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+      hasNextPage: page * limit < total,
+      hasPreviousPage: page > 1,
+    };
+  }
+}
