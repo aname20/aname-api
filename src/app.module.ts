12350b635fcea827005eb007d1c49a8470341f13
LucasLuis-Dev <lucasluisouza@gmail.com>
2025-12-07 14:30:02 -0300

    fix: adjustments to the prisma service configuration to prevent prisma initialization errors with .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,20 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { PrescriptionsModule } from './prescriptions/prescriptions.module';
+import { ConfigModule } from '@nestjs/config';
 
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [UsersModule, PrismaModule, AuthModule],
-  imports: [UsersModule, PrismaModule, PrescriptionsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    PrismaModule,
+    AuthModule,
+    PrescriptionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
